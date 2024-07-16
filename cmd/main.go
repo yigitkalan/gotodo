ed30d6eeefa8329bc04b1be304bd66500b74848e
yigitkalan <sametykalan@gmail.com>
2024-07-17 01:26:25 +0300

    feat: add initial structure

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,66 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Operation int8
+
+const (
+    Add Operation = iota
+    Remove 
+    Complete
+    List
+    Exit
+    Help
+)
+
 func main(){
+    fmt.Println("Welcome to the task manager!")
+    fmt.Println("Enter 'help' to see the list of commands")
+    scanner := bufio.NewScanner(os.Stdin)
+    for scanner.Scan() {
+    }
+}
+
+func (operation Operation) Operate(){
+    switch operation {
+    case Add:
+        AddTask()
+    case Complete:
+        CompleteTask()
+	case Exit:
+        os.Exit(0)
+	case Help:
+        ShowHelp()
+	case List:
+        ListTasks()
+	case Remove:
+        RemoveTask()
+	default:
+		panic(fmt.Sprintf("unexpected main.Operation: %#v", operation))
+	}
+}
+
+func RemoveTask() {
+	panic("unimplemented")
+}
+
+func ListTasks() {
+	panic("unimplemented")
+}
+
+func ShowHelp() {
+	panic("unimplemented")
+}
+
+func CompleteTask() {
+	panic("unimplemented")
+}
+
+func AddTask() {
+	panic("unimplemented")
 }
